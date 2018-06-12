1318e2fab4ff79fc06c82e241d0b788663da5f06
vishvanatarajan <vishvanatarajan@gmail.com>
2018-06-12 16:34:47 +0530

    Untested code for finding latitude and longitude using GPS

--- app/src/main/java/com/anurag/geolocationdemo/MainActivity.java
+++ app/src/main/java/com/anurag/geolocationdemo/MainActivity.java
@@ -2,69 +2,50 @@ package com.anurag.geolocationdemo;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
+    private String provider;
     private LocationManager locationManager;
-    private LocationListener locationListener;
+    private TextView latValTV, longValTV;
+    public static final int MY_PERMISSION_REQUEST_LOCATION = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        latValTV = findViewById(R.id.latValTV);
+        longValTV = findViewById(R.id.longValTV);
+
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
 
-                String longitude = String.valueOf(location.getLongitude());
-                String latitude = String.valueOf(location.getLatitude());
-                String loc = latitude + ", " + longitude;
-                Log.i("Location of phone",loc);
-                Toast.makeText(MainActivity.this, loc, Toast.LENGTH_SHORT).show();
+        provider = locationManager.getBestProvider(new Criteria(), false);
 
-            }
+        checkLocationPermission();
 
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-
-            //Ask for permission
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-
+        Location location = locationManager.getLastKnownLocation(provider);
+        if(location != null) {
+            latValTV.setText(String.format("%s", location.getLatitude()));
+            longValTV.setText(String.format("%s", location.getLongitude()));
         }
-        else{
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
-
+        else {
+            latValTV.setText(R.string.location_failure);
+            longValTV.setText(R.string.location_failure);
         }
-
     }
 
     @Override
@@ -72,12 +53,79 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+                locationManager.requestLocationUpdates(provider, 5000, 10, this);
             }
+        }
+        else {
+            latValTV.setText(R.string.permission_not_provided);
+            longValTV.setText(R.string.permission_not_provided);
         }
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location != null) {
+            latValTV.setText(String.format("%s", location.getLatitude()));
+            longValTV.setText(String.format("%s", location.getLongitude()));
+        }
+        else {
+            latValTV.setText(R.string.location_failure);
+            longValTV.setText(R.string.location_failure);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.removeUpdates(this);
+        }
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage(R.string.rationale_location_permission)
+                        .setPositiveButton(R.string.positive_response, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+            }
+            return false;
+        }
+        return true;
+    }
 }
